d92b00b1af265ab726ea5ef918136532ea959849
Nate Ferrell <nate@scrthq.com>
2019-07-05 23:42:24 -0500

    updated loadAssemblies.ps1

--- loadAssemblies.ps1
+++ loadAssemblies.ps1
@@ -1,13 +1,13 @@
 try {
-    $dllpath = [System.IO.Path]::Combine($PSScriptRoot,'BouncyCastle.Crypto.dll')
-    Add-Type -Path $dllpath | Out-Null
+    $bouncyCastleDll = [System.IO.Path]::Combine($PSScriptRoot,'BouncyCastle.Crypto.dll')
+    Add-Type -Path $bouncyCastleDll | Out-Null
 }
 catch {
     $global:Error.Remove($global:Error[0])
 }
 try {
-    $dllpath = [System.IO.Path]::Combine($PSScriptRoot,'SCRTHQ.PEMEncrypt','bin','Debug','netstandard2.0','SCRTHQ.PEMEncrypt.dll')
-    Add-Type -Path $dllpath | Out-Null
+    $PEMEncrypt = [System.IO.Path]::Combine($PSScriptRoot,'SCRTHQ.PEMEncrypt','bin','Debug','netstandard2.0','SCRTHQ.PEMEncrypt.dll')
+    Add-Type -Path $PEMEncrypt -ReferencedAssemblies $bouncyCastleDll | Out-Null
 }
 catch {
     $global:Error.Remove($global:Error[0])
