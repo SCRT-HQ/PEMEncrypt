1a2613bbc529c0ed05cccf7597d159cc5c98451a
Nate Ferrell <nate@scrthq.com>
2019-07-07 22:21:51 -0500

    pushing updates with documenation and standard project documentation

--- PEMEncrypt/Public/Protect-PEMString.ps1
+++ PEMEncrypt/Public/Protect-PEMString.ps1
@@ -1,27 +1,58 @@
 function Protect-PEMString {
+    <#
+    .SYNOPSIS
+    Encrypts a string by using a public RSA key.
+
+    .DESCRIPTION
+    Encrypts a string by using a public RSA key.
+
+    .PARAMETER StringToEncrypt
+    The plain-text string that you would like to encrypt with the public key.
+
+    .PARAMETER PublicKey
+    The full or relative path to the public key OR the key itself in string format.
+
+    .EXAMPLE
+    # Using a password-less private key
+    $encrypted = 'Hello','How are you today?' | Protect-PEMString -PublicKey .\public.pem
+    $encrypted | Unprotect-PEMString -PrivateKey .\private.pem
+
+    .EXAMPLE
+    # Use Get-Credential to prompt for credentials so it's not in plain text
+    $encrypted = 'Hello','How are you today?' | Protect-PEMString -PublicKey .\public_des3.pem
+    $keyCreds = Get-Credential -UserName key -Message 'Please enter the password for the private key'
+    $encrypted | Unprotect-PEMString -PrivateKey .\private_des3.pem -Password $keyCreds.Password
+
+    .EXAMPLE
+    # Build a SecureString using a plain-text password
+    $encrypted = 'Hello','How are you today?' | Protect-PEMString -PublicKey .\public_des3.pem
+    $password = ConvertTo-SecureString 'P@$$w0rd' -AsPlainText -Force
+    $encrypted | Unprotect-PEMString -PrivateKey .\private_des3.pem -Password $password
+    #>
     [OutputType('System.String')]
     [CmdletBinding()]
     Param (
         [parameter(Mandatory,Position = 0,ValueFromPipeline)]
+        [Alias('String')]
         [String[]]
         $StringToEncrypt,
         [parameter(Mandatory,Position = 1)]
+        [Alias('PublicKeyPath','Key')]
         [String]
-        $PublicKeyPath,
-        [parameter(Position = 2)]
-        [Int]
-        $KeySize = 2048
+        $PublicKey
     )
     Begin {
         Import-Assemblies
+        if (Test-Path $PublicKey -IsValid) {
+            $PublicKey = ([System.IO.File]::ReadAllText((Resolve-Path $PublicKey).Path))
+        }
     }
     Process {
         foreach ($string in $StringToEncrypt) {
             try {
                 [SCRTHQ.PEMEncrypt.Crypto]::Encrypt(
                     $string,
-                    ([System.IO.File]::ReadAllText((Resolve-Path $PublicKeyPath).Path)),
-                    $KeySize
+                    $PublicKey
                 )
             }
             catch {
